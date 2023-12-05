97e45d62c6cdca8a92c87cb5acb926d49b31c8ed
Renier Jordan <renier.jordan@hotmail.com>
2023-12-05 18:08:17 +0100

    Modal changes

--- example/src/App.js
+++ example/src/App.js
@@ -1,30 +1,18 @@
 import React, { useState } from 'react'
+import Modal from 'simple-modal-jr-lib'
+import 'simple-modal-jr-lib/dist/index.css'
 
-import { Modal } from 'simple-modal-jr-lib'
 export default function App() {
-  const [isOpen, setIsOpen] = useState(false) // this hook will determine when the modal opens or closes
+  const [openModal, setOpenModal] = useState(false) // this hook will determine when the modal opens or closes
   return (
     <div className='App'>
       <h1>Modal Test</h1>
-      <button onClick={() => setIsOpen(true)}>Open Modal</button>{' '}
-      {/* We set the isOpen hook on true to open the modal when we click on the button */}
-      {isOpen && (
-        <Modal
-          setIsOpen={setIsOpen}
-          text={'Hello World !'}
-          modalBgColor={'green'}
-          modalBorder={'3px solid purple'}
-          modalBorderRadius={'20px'}
-          crossCloseBg={'red'}
-          crossCloseColor={'white'}
-          crossCloseBorder={'3px solid pink'}
-          fontFamily={'Impact'}
-          fontSize={'20px'}
-          fontColor={'white'}
-          textAlign={'center'}
-        />
-      )}{' '}
-      {/* The modal component needs at least the 'setIsOpen' and 'text' props  */}
+      <button onClick={() => setOpenModal(true)}>Open Modal</button>{' '}
+      {/* We set the OpenModal hook on true to open the modal when we click on the button */}
+      <Modal isOpen={openModal} onClose={() => setOpenModal(false)}>
+        <p>Exemple text !</p>
+      </Modal>
+      {/* The modal component needs at least the 'setIsOpen' and the onClose function  */}
     </div>
   )
 }
